e047237ae0123f0ecdf04cae55fa8013ad745d4a
klonirovan89 <klonirovan89@gmail.com>
2024-01-29 21:25:14 +0300

    added HeaderMenu component commit 1

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -4,6 +4,7 @@ import {Logo} from "../../components/Logo/Logo";
 import {Menu} from "../../components/Menu/Menu";
 import {Container} from "../../components/Container";
 import {FlexWrapper} from "../../components/FlexWrapper";
+import {HeaderMenu} from "./headerMenu/HeaderMenu";
 
 
 const items = ["Home", "Skills", "Works", "Testimony", "Contact"]
@@ -16,7 +17,7 @@ export const Header = () => {
             <Container>
                 <FlexWrapper justify={'space-between'} align={'center'}>
                     <Logo/>
-                    <Menu menuItems={items}/>
+                    <HeaderMenu menuItems={items}/>
                 </FlexWrapper>
 
             </Container>
@@ -25,7 +26,11 @@ export const Header = () => {
 };
 
 const StyledHeader = styled.header`
-    background: green;
-    display: flex;
-    justify-content: space-between;
+    background: rgba(31, 31, 32, 0.9);
+    padding: 20px 0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 99999;
 `
